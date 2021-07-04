96419df1a12157aeac4f7fb2c52dd5134f69392e
tyler <baile320@umn.edu>
2021-07-04 09:23:53 -0500

    do ch 9

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,3 +1,37 @@
+use std::fs::File;
+use std::io;
+use std::io::ErrorKind;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Error creating the file: {:?}", e),
+            },
+            _other_error => panic!("Problem opening the file: {:?}", error),
+        },
+    };
+
+    let blah = String::new();
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
 }
