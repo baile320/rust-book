d876412c4aa9d4cdb104dac8f5500e0991a20e66
tyler <baile320@umn.edu>
2021-07-05 09:26:28 -0500

    add tests to minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -25,3 +25,38 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn config_new_should_succeed() {
+        let config = Config::new(&[
+            String::from("path"),
+            String::from("query"),
+            String::from("filename"),
+        ])
+        .unwrap();
+
+        assert_eq!(config.query, "query");
+        assert_eq!(config.filename, "filename");
+    }
+
+    #[test]
+    fn config_should_be_err_without_args() {
+        let config_err = Config::new(&[]).is_err();
+        assert_eq!(config_err, true);
+    }
+
+    #[test]
+    fn run_should_fail_with_inexistent_file() {
+        let run_is_err = run(Config {
+            query: String::from("Some search query"),
+            filename: String::from("ThisFileDefinitelyDoesntExist.txt"),
+        })
+        .is_err();
+
+        assert_eq!(run_is_err, true);
+    }
+}
