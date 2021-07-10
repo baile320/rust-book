1078c0e2aa61fdd2c6fc84517e697cb8052f33bb
tyler <baile320@umn.edu>
2021-07-10 13:19:16 -0500

    complete ch 18

---
+++ pattern_matching/src/main.rs
@@ -0,0 +1,61 @@
+struct Point {
+    x: u8,
+    y: u8,
+}
+
+enum Message {
+    Hello { id: i32 },
+}
+
+fn main() {
+    let p = Point { x: 7, y: 7 };
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } if x == y => println!("On line y = x: ({}, {})", x, y),
+        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    }
+
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+
+    println!("setting is {:?}", setting_value);
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, _, third, _, fifth) => {
+            println!("Some numbers: {}, {}, {}", first, third, fifth)
+        }
+    }
+
+    let s = Some(String::from("Hello!"));
+
+    if let Some(_) = s {
+        println!("found a string");
+    }
+
+    println!("{:?}", s);
+
+    let msg = Message::Hello { id: 5 };
+
+    match msg {
+        Message::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {}", id_variable),
+        Message::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        Message::Hello { id } => println!("Found some other id: {}", id),
+    }
+}
