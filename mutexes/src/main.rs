07a6b926b965c9a5a36452ad0c40591710d0ee63
tyler <baile320@umn.edu>
2021-07-10 07:00:19 -0500

    do mutex section

--- mutexes/src/main.rs
+++ mutexes/src/main.rs
@@ -1,3 +1,49 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+
+    // example of splitting work into multiple worker threads. summing 0 - 299
+    let accumulator = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    let num_ranges = vec![0..100, 100..200, 200..300];
+
+    for num_range in num_ranges {
+        let accumulator = Arc::clone(&accumulator);
+        let handle = thread::spawn(move || {
+            let mut sum = accumulator.lock().unwrap();
+            for num in num_range {
+                *sum += num
+            }
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!(
+        "The sum of the first 300 nums is: {}",
+        *accumulator.lock().unwrap()
+    );
 }
